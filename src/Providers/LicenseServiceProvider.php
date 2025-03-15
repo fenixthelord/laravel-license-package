2fab504b6eb5a7bb3f0c190391cca360261b97ce
Muhammad <fenix.p2h@hotmail.com>
2025-03-15 18:38:33 +0300

    Fix Namespace

--- src/Providers/LicenseServiceProvider.php
+++ src/Providers/LicenseServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Fenixthelord\LaravelLicense\Providers;
 
-
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\App;
 use Illuminate\Contracts\Container\BindingResolutionException;
@@ -17,14 +16,16 @@ class LicenseServiceProvider extends ServiceProvider
     public function boot()
     {
         // تحقق من وجود البكج
-        if (!App::bound(Fenixthelord\License\LicenseServiceProvider::class)) {
-            abort(403,'The license management package is missing. The application cannot run.');
+        if (!App::has(Fenixthelord\LaravelLicense\Providers\LicenseServiceProvider::class)) {
+            exit('The license management package is missing. The application cannot run.');
         }
-         $this->app['router']->aliasMiddleware('checkLicense', \Fenixthelord\LaravelLicense\Http\Middleware\CheckLicense::class);
 
-         // إعداد الـ Middleware للعميل
+        // تسجيل الـ Middleware
+        $this->app['router']->aliasMiddleware('checkLicense', \Fenixthelord\LaravelLicense\Http\Middleware\CheckLicense::class);
+
+        // إعداد الـ Middleware للعميل
         if (config('laravel-license.mode') === 'client') {
-            $this->app['router']->aliasMiddleware('license', CheckLicense::class);
+            $this->app['router']->aliasMiddleware('license', \Fenixthelord\LaravelLicense\Http\Middleware\CheckLicense::class);
         }
 
         // تحميل الترحيلات والطرق للسيرفر
@@ -33,6 +34,7 @@ class LicenseServiceProvider extends ServiceProvider
             $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
         }
 
+        // نشر الإعدادات والملفات الخاصة بالبكج
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../config/laravel-license.php' => config_path('laravel-license.php'),
