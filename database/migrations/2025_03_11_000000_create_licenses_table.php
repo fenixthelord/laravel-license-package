54067e1010dcb8f7491357860a296fb65b0aff51
Muhammad <fenix.p2h@hotmail.com>
2025-03-16 10:00:12 +0300

    Fix Namespace

--- database/migrations/2025_03_11_000000_create_licenses_table.php
+++ database/migrations/2025_03_11_000000_create_licenses_table.php
@@ -4,9 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLicensesTable extends Migration
-{
-    public function up()
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('licenses', function (Blueprint $table) {
             $table->id();
@@ -19,8 +21,11 @@ class CreateLicensesTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('licenses');
     }
-}
+};
